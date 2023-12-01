166bf882cab1fc41a45dfa7867eef1a6f5a81214
Jacob Lundström <113439800+JacobLundstrom@users.noreply.github.com>
2023-12-01 10:40:19 +0100

    test files and jest installed

---
+++ Tests/DiscountedPrice.test.js
@@ -0,0 +1,8 @@
+import { discountedPrice } from './index.html';
+
+describe('DiscountedPrice', () => {
+  it('should return the discounted price', () => {
+    // Detta är själva testet
+    expect(discountedPrice).toBe(2);
+  });
+});
